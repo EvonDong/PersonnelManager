0050d67c01e30a2dc762d393f4911d1b4d446e42
EvonDong <e0322896@u.nus.edu>
2020-08-24 21:38:09 +0800

    Change landing page

--- backend/index.js
+++ backend/index.js
@@ -41,7 +41,7 @@ app.use('/api', apiRoutes);
 
 // Handle production
 if(process.env.NODE_ENV === "production") {            // true when deploy to heroku
-    app.use(express.static(__dirname + '/public/', {index: 'index.html'}));
+    app.use(express.static(__dirname + '/public/'));
 
     // Handle SPA(Single Page Application)
     // app.get(/.*/, (req, res) => res.sendFile(__dirname + '/public/index.html'));                       // refer to any route
